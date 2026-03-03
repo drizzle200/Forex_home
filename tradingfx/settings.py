303320ff16ac77bb294bfeb282e1d9e41448056f
drizzle200 <drizzleboe@gmail.com>
2026-03-03 13:13:09 +0300

    debug false

--- tradingfx/settings.py
+++ tradingfx/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-n8daet-k0j71klc-g=mz610sxpz3i20d=y-pjabh#k*7xgow_j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  'True'
+DEBUG =  'False'
 
 # Allow Fly.io domain and local
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.fly.dev').split(',')
